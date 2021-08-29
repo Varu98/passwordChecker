a39ee9a419ac8c7d133dfe3ffec9bfa97d868aae
Vardaan <vardaan.agarwal.98@gmail.com>
2021-08-29 17:40:58 +0530

    pair code I/O

---
+++ index.js
@@ -0,0 +1,7 @@
+//  Create a password checker web app. If password is lesser than 10 characters then show an error to user otherwise show success. Someone can ask to make the submit button disabled. Some can ask to make the input field green or red depending on input.
+const input = document.querySelector("#password");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function onClick() {
+  console.log("hello");
+});
